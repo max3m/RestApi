189187d6ab49f80823089b177ce00e289a139e71
max3m <max-samara120@mail.ru>
2021-06-15 17:19:47 +0300

    removed comments

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -33,39 +33,3 @@ CREATE TABLE  user_roles (
 
 )
     ENGINE  = InnoDB;
-
-/*# -- Table: users
-CREATE TABLE users(
-                      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                      username VARCHAR(30) UNIQUE,
-                      password VARCHAR(80),
-                      firstname VARCHAR(50),
-                      lastname VARCHAR(50),
-                      age INT,
-                      email VARCHAR(50) UNIQUE
-
-)
-    ENGINE  = InnoDB;
-
-
-
--- Table: roles
-CREATE TABLE roles (
-                       id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                       rolename VARCHAR(50)
-)
-    ENGINE  = InnoDB;
-
-
--- Table: for mapping user add roles: user_roles
-CREATE TABLE  users_roles (
-                             user_id BIGINT,
-                             role_id BIGINT,
-
-                             FOREIGN KEY (user_id) REFERENCES users(id),
-                             FOREIGN KEY (role_id) REFERENCES roles(id),
-
-                             UNIQUE (user_id, role_id)
-
-)
-    ENGINE  = InnoDB;*/

855230703be939c69682b6da244442b4bab4cfad
max3m <max-samara120@mail.ru>
2021-06-15 17:18:56 +0300

    removed comments

--- src/main/java/ru/javamentor/restapi/RestApiApplication.java
+++ src/main/java/ru/javamentor/restapi/RestApiApplication.java
@@ -1,11 +1,7 @@
 package ru.javamentor.restapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.javamentor.restapi.web.model.User;
-import ru.javamentor.restapi.web.model.dto.UserDTO;
-import ru.javamentor.restapi.web.service.UserService;
 
 @SpringBootApplication
 public class RestApiApplication {
--- src/main/java/ru/javamentor/restapi/web/repository/RoleRepository.java
+++ src/main/java/ru/javamentor/restapi/web/repository/RoleRepository.java
@@ -1,6 +1,5 @@
 package ru.javamentor.restapi.web.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javamentor.restapi.web.model.Role;
 
